bcb40d0d622aa9e9b27ec38d90f4e3009f7476e5
Santiago Dubon <santiagodubon@outlook.com>
2022-12-16 23:14:06 -0500

    Added stuff

--- book.js
+++ book.js
@@ -9,33 +9,55 @@ function Book (title, author, pages, read) {
     this.read = read
 }
 
-Book.prototype = {
-    info() {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read
-    }
-}
-
-
 // FUNCTIONS
 function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+const toggleRead = (e) => {
+    console.log(e.read)
+
+    if (e.read === true) {
+        e.read = false
+    } else {
+        e.read = true
+    }
+
+    console.log(e.read)
+}
+
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+let theHobbit2 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+let theHobbit3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+let theHobbit4 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
 addBookToLibrary(theHobbit)
+addBookToLibrary(theHobbit2)
+addBookToLibrary(theHobbit3)
+addBookToLibrary(theHobbit4)
+
 
 myLibrary.forEach(function(book) {
     // ACTUAL CARD
     const card = document.createElement("div")
 
     // STYLING FOR CARD
-    card.classList.add("flex", "flex-col", "text-white", "bg-gray-700", "rounded-lg", "min-w-fit")
+    card.classList.add("flex", "flex-col", "text-white", "bg-gray-700", "rounded-lg", "w-40", "h-40")
 
     // CARD ELEMENTS
-    const title = document.createElement('p')
+    const title = document.createElement('h1')
+    title.classList.add("card-title")
+
     const author = document.createElement('p')
+    author.classList.add("author")
+
     const pages = document.createElement('p')
-    const isRead = document.createElement('p')
+    pages.classList.add("pages")
+
+    const isRead = document.createElement('button')
+    isRead.classList.add("read-btn")
+    isRead.onclick = function() {
+        toggleRead(book)
+    }
 
     // GIVE CARD ELEMENTS THINGS
     title.innerHTML = book.title
@@ -51,13 +73,8 @@ myLibrary.forEach(function(book) {
 
     // ADD STUFF TO THE CARD-CONTAINER
     const container = document.getElementById('card-container')
+    container.classList.add("h-screen", "flex", "flex-row", "gap-10", "m-10")
     container.appendChild(card)
-
-    const p = document.getElementsByTagName('p')
-    console.log(p)
-    p.forEach(function(element) {
-        element.classList.add("w-3/4")
-    })
 })
 
 console.log(myLibrary)
